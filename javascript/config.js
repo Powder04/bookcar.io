813aea535a54689209f922ff3635f95015841466
MyTran <thaomy.cma@gmail.com>
2026-02-02 00:19:44 +0700

    rm phone_display var

--- javascript/config.js
+++ javascript/config.js
@@ -1,4 +1,3 @@
 export const SITE_NAME = "HOÀNG PHÚ";
 export const PHONE = "0911444327";
-export const PHONE_DISPLAY = "0911 444 327";
 export const ZALO_PHONE = PHONE;
--- javascript/main.js
+++ javascript/main.js
@@ -5,13 +5,18 @@ import { initCarousel } from "./carousel.js";
 import { initForm, showForm, closeForm } from "./form.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+
+  function formatPhoneDisplay(phone) {
+    return phone.replace(/(\d{4})(\d{3})(\d{3})/, "$1 $2 $3");
+  }
+
   // Chen Site name
   document.querySelectorAll(".owner").forEach((el) => (el.textContent = Data.SITE_NAME));
 
   // Chen sdt va zalo
   document.querySelectorAll(".phone-vr").forEach((tel) => (tel.href = `tel:${Data.PHONE}`));
   document.querySelector(".zalo-vr").href = `https://zalo.me/${Data.ZALO_PHONE}`;
-  document.querySelector(".phone_display").innerHTML = Data.PHONE_DISPLAY;
+  document.querySelector(".phone_display").innerHTML = formatPhoneDisplay(Data.PHONE);
 
   initTheme();
   initAnimation();
